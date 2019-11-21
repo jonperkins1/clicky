b89c450b366df52b6b121943130e278876e44969
Jon Perkins <joso1958@gmail.com>
2019-11-20 23:49:24 -0500

    updated github fix'

---
+++ build/precache-manifest.3600c524f895f7964692fe73c9770605.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "792e3e6602896ea3faf8044101cf7f1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c46557ddcb2ccf5e9b6",
+    "url": "/static/css/main.4457679e.chunk.css"
+  },
+  {
+    "revision": "03fb607d643e88c8761d",
+    "url": "/static/js/2.b18b6a88.chunk.js"
+  },
+  {
+    "revision": "7c46557ddcb2ccf5e9b6",
+    "url": "/static/js/main.5c93ef5a.chunk.js"
+  },
+  {
+    "revision": "4bcb312e388791136467",
+    "url": "/static/js/runtime-main.75ac34d6.js"
+  }
+]);
